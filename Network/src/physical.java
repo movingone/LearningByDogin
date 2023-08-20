76ab05f933b802d2301c8974368628fc3e238742
동원 <ai06021@gmail.com>
2023-08-20 21:00:49 +0900

    create : physical

---
+++ Network/src/physical.java
@@ -0,0 +1,29 @@
+package Network.src;
+
+public class physical {
+    private int bitstream[];
+
+    physical(int bitstream[]) {
+        this.bitstream = bitstream;
+    }
+}
+
+
+
+class Packet {
+    int header;
+    int data;
+
+}
+
+class network {
+
+}
+
+class transport {
+
+}
+
+class session {
+
+}
